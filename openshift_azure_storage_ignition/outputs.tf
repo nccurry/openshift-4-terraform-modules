bcb09d962622449552b55c551adeb2d1f40d4742
nccurry <chasecurry@gmail.com>
2019-12-19 10:14:02 -0600

    Fix misspelling

--- openshift_azure_storage_ignition/outputs.tf
+++ openshift_azure_storage_ignition/outputs.tf
@@ -1,11 +1,11 @@
-outout "bootstrap_redirect_ignition_source" {
+output "bootstrap_redirect_ignition_source" {
   value = "${azurerm_storage_blob.bootstrap_ignition.url}${data.azurerm_storage_account_sas.bootstrap_ignition.sas}"
 }
 
-outout "master_redirect_ignition_source" {
+output "master_redirect_ignition_source" {
   value = "${azurerm_storage_blob.master_ignition.url}${data.azurerm_storage_account_sas.master_ignition.sas}"
 }
 
-outout "worker_redirect_ignition_source" {
+output "worker_redirect_ignition_source" {
   value = "${azurerm_storage_blob.worker_ignition.url}${data.azurerm_storage_account_sas.worker_ignition.sas}"
 }
