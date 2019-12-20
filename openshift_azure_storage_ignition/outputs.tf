7bf676b8b62653fc57840dd8603530b000b41026
nccurry <chasecurry@gmail.com>
2019-12-20 09:27:40 -0600

    Fix syntax error, change output names to match cluster module/playbook

--- openshift_azure_storage_ignition/outputs.tf
+++ openshift_azure_storage_ignition/outputs.tf
@@ -1,11 +1,11 @@
-output "bootstrap_redirect_ignition_source" {
+output "bootstrap_ignition_source_uri" {
   value = "${azurerm_storage_blob.bootstrap_ignition.url}${data.azurerm_storage_account_sas.bootstrap_ignition.sas}"
 }
 
-output "master_redirect_ignition_source" {
+output "master_ignition_source_uri" {
   value = "${azurerm_storage_blob.master_ignition.url}${data.azurerm_storage_account_sas.master_ignition.sas}"
 }
 
-output "worker_redirect_ignition_source" {
+output "worker_ignition_source_uri" {
   value = "${azurerm_storage_blob.worker_ignition.url}${data.azurerm_storage_account_sas.worker_ignition.sas}"
 }
